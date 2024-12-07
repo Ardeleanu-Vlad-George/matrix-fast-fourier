9af62f3b4be723d44a8c71d6a605b3b307ce2316
Ardeleanu-Vlad-George <ardeleanu_vlad@ymail.com>
2024-12-07 17:05:06 +0200

    Made progress on this project, but need to rework the previous one

--- src/fft_comp.c
+++ src/fft_comp.c
@@ -3,15 +3,26 @@
 #include "fft_prep_cpx.h"
 #include "cpx_op.h"
 
-void fft_order(int nr, int pwr, double *vct){
-  int it;
+void fft_order_one(int type, int nr, int pwr, double *vct){
+  //type: 0 means line, 1 means column
+  int iter;
   double buff[2*nr];
 
-  for(it=0; it < nr; it++)
-    asn(buff+2*it, vct+2*it);
+  for(iter=0; iter < nr; iter+=(type ? nr : 1))
+    asn(buff+2*iter, vct+2*iter);
 
-  for(it=0; it < nr; it++)
-    asn(vct+2*it, buff+2*revidx(it, pwr));
+  for(iter=0; iter < nr; iter+=(type ? nr : 1))
+    asn(vct+2*iter, buff+2*revidx(iter, pwr));
+}
+
+void fft_order(int nr, int pwr, double *vct){
+  int iter;
+
+  for(iter=0; iter < nr; iter++)
+    fft_order_one(0, nr, pwr, vct+2*iter*nr);
+
+  for(iter=0; iter < nr; iter++)
+    fft_order_one(1, nr, pwr, vct+2*iter);
 }
 /**
  * Consider the next equations making up the butterfly schema:
@@ -42,79 +53,6 @@ void pair_butterfly(double *oonn, double *neun, double *oonn_coef, double *neun_
     )
   );
 }
-/**
-    __OBS__:
-    This is the most important function in the whole program, the namesake of the project.
-    It's paramount that the explanation is clear and thoughtful.
-    
-    __Variables__:
-    @nr <-> the length of the complex vector
-    @pwr <-> the logarithm of 2 of 'nr'
-    @vct <-> the complex vector
-
-    __Implement__:
-    There are two aspects of it that should be clarified:
-    1. the layer structure,
-    2. the BFS (ButterFly Schema).
-    
-    Both are explained in the following subsections.  
-    
-    __Layers__:
-    The FFT could be seen as applying the BFS multiple times, until we reach the desired result. 
-    A classic BFS has inputs and outputs, here multiple BFSs will be grouped in a _layer_, it's compounded outputs being fed into the inputs of a similar _layer_.
-    This gives the algorithm's layered aspect, the input of the first layer being 'vct' and the output of the final layer being the computed FFT.
-    Each layer of schemas (SL - Schema Layer) has a corresponding layer of numbers at input (NL - Number Layer), forming together a WL (Whole Layer).
-    Each NL will be  
-    
-
-
-    The following explanation for the layer structure is inspired by the one found in 'docu/link/FAST_FOURIER_TRANSFORM.pdf', especially the schema 
-    Layer structure: 
-    - the computations can be viewed as a process that feeds information from one layer to the next, each layer having 'nr'
-    
-    , the butterfly computations
-    
-    , explained 
-*/
-
-//Keeps this definition of the function here, maybe later you will try 
-//to measure the efficiency of the two variants
-// void fft_apply(int nr, int pwr, double *vct, double *rts){
-//     double *rtEvn, *rtOdd;
-//         
-//     int layer, seqLn, pwStp;
-//     int elmId, seqId;
-//     double *oddOut, *evnOut,
-//            oddInp[2], evnInp[2],
-//            temp[2];
-//     int oddId, evnId;
-//
-//     for(layer=0, seqLn=1, pwStp=nr/2;
-//         layer < pwr;
-//         layer++, seqLn*=2, pwStp/=2)
-//             for(seqId=0; seqId < nr/seqLn; seqId+=2)
-//                 for(elmId=0; elmId < seqLn; elmId++){
-//                     
-//                     evnId = seqId*seqLn+elmId;
-//                     asn(evnInp, vct+2*evnId);
-//                     evnOut = vct+2*evnId;
-//                     rtEvn = rts+((evnId*pwStp)%nr)*2;
-//
-//                     oddId = evnId+seqLn;
-//                     asn(oddInp, vct+2*oddId);
-//                     oddOut = vct+2*oddId;
-//                     rtOdd = rts+((oddId*pwStp)%nr)*2;
-//
-//                     add( evnOut, evnInp,
-//                         tms( temp, oddInp, rtEvn)
-//                     );
-//
-//                     add( oddOut, evnInp,
-//                         tms( temp, oddInp, rtOdd)
-//                     );
-//
-//                 }
-// }
 
 void seqn_butterfly(double *pair, int lenf, double *ruts, int step){
   double  *oonn_seqn = pair, *neun_seqn = pair+2*lenf, 
@@ -131,7 +69,8 @@ void seqn_butterfly(double *pair, int lenf, double *ruts, int step){
     );
 }
 
-void fft_apply(int nr, int pwr, double *vct, double *rts){
+void fft_apply_one(int type, int nr, int pwr, double *vct, double *rts){
+  //0 - means line, 1 - means column
   double *seqn_pair, *vect_stop;
   vect_stop = vct+2*nr;
   int seqn_lenf, layer_cnt, powr_step; 
@@ -142,4 +81,14 @@ void fft_apply(int nr, int pwr, double *vct, double *rts){
     layer_cnt++, seqn_lenf*=2, powr_step/=2
   )for(seqn_pair=vct; seqn_pair < vect_stop; seqn_pair+=4*seqn_lenf)
     seqn_butterfly(seqn_pair, seqn_lenf, rts, powr_step);
+}
+
+void fft_apply(int nr, int pwr, double *vct, double *rts){
+  int iter;
+
+  for(iter=0; iter < nr; iter++)
+    fft_apply_one(0, nr, pwr, vct+2*iter*nr, rts);
+
+  for(iter=0; iter < nr; iter++)
+    fft_apply_one(1, nr, pwr, vct+2*iter, rts);
 }
--- src/main.c
+++ src/main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "io.h"
+#include "fft_comp.h"
+#include "fft_prep_bit.h"
+
 
 int main(){
   double **mtr;
-  int size;
+  int size, powr;
   mtr = read("data/in", &size);
+  powr = getexp(size);
+  fft_order(size, powr, *mtr);
   write("./data/out", size, mtr);
+
   free(*mtr);
   free(mtr);
   return 0;
